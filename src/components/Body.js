4e3d7175b3f458378ef08150dc277e5b9f2666e2
vyshnavichidire <vyshnavichidire@gmail.com>
2024-05-08 18:27:16 +0530

    commit

---
+++ src/components/Body.js
@@ -0,0 +1,17 @@
+
+import Header from './Header';
+import Maincontainer from './Maincontainer';
+import Sidebar from './Sidebar';
+import {Outlet } from 'react-router-dom'; 
+
+function Body() {
+  return (
+    <div className=' grid grid-flow-col mt-16 w-full'>
+      <Header />
+      <Sidebar />
+      <Outlet/> 
+    </div>
+  )
+}
+
+export default Body
